72669924078d00c9c04b3faf108ffd6a856640a9
aowubulao <me@neow.cc>
2021-07-08 21:05:40 +0800

    Initial commit

---
+++ Podenco-application/src/main/java/com/neoniou/bot/PodencoApplication.java
@@ -0,0 +1,12 @@
+package com.neoniou.bot;
+
+/**
+ * @author Neo.Zzj
+ * @date 2021/7/8
+ */
+public class PodencoApplication {
+
+    public static void main(String[] args) {
+        PodencoCore.run(PodencoApplication.class);
+    }
+}
---
+++ Podenco-core/src/main/java/com/neoniou/bot/PodencoCore.java
@@ -0,0 +1,12 @@
+package com.neoniou.bot;
+
+/**
+ * @author Neo.Zzj
+ * @date 2021/7/8
+ */
+public class PodencoCore {
+
+    public static void run(Class<?> mainClass) {
+
+    }
+}
