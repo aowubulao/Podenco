c9b7ef79b2dbca1bfcefa334b77137e75bb32c3d
aowubulao <--global>
2021-09-11 16:25:10 +0800

    chore: update mirai version

--- Podenco-application/src/main/java/com/neoniou/bot/handler/SampleHandler.java
+++ Podenco-application/src/main/java/com/neoniou/bot/handler/SampleHandler.java
@@ -11,17 +11,17 @@ import net.mamoe.mirai.message.data.Image;
  * @date 2021/7/8
  */
 @BotHandler(name = "Sample")
-public class SampleHandler {
+public class SampleHandler extends MessageUtil {
 
     @BotHandler(name = "replyHelloMapping", matchStr = "hello")
     public void replyHelloMapping(BotMessage<MessageEvent> message) {
         Image image = MessageUtil.uploadImage(
                 "https://img.neoniou.com/blog/20210820202649.png", message.getEvent());
-        MessageUtil.sendMessage(message, image);
+        sendMessage(message, image);
     }
 
     @BotHandler(name = "hello2", matchStr = "HelloWorld2")
     public void sample2(BotMessage<MessageEvent> message) {
-        MessageUtil.sendMessage(message, "Hello World!");
+        sendMessage(message, "Hello World!");
     }
 }
